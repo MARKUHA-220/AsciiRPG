baf23f08bf544fa19e1582a9f63857a73bae1926
Ričiard Kozlovskij <markasm133@gmail.com>
2026-03-02 10:08:15 +0200

    Delete build-exe.bat

--- build-exe.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-
-dotnet publish AsciiRPG.csproj -c Release -r win-x64 --self-contained true /p:PublishSingleFile=true
-if errorlevel 1 exit /b 1
-
-echo EXE собран: bin\Release\net8.0\win-x64\publish\AsciiRPG.exe
